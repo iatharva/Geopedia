3809500ae8bccb5ba1e3a556e968566d1b54f8e7
Atharva Ikhar <atharva464@gmail.com>
2022-08-15 16:50:43 +0530

    BACKEND: Added menu

--- app/src/main/java/com/example/geopedia/adminmenu/Information/events/EventsFragment.java
+++ app/src/main/java/com/example/geopedia/adminmenu/Information/events/EventsFragment.java
@@ -2,21 +2,29 @@ package com.example.geopedia.adminmenu.Information.events;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.geopedia.CommentFeed;
+import com.example.geopedia.Info.EventInfo;
+import com.example.geopedia.Info.QuestionInfo;
 import com.example.geopedia.R;
 import com.example.geopedia.databinding.FragmentEventsBinding;
 import com.example.geopedia.extras.Events;
@@ -96,16 +104,85 @@ public class EventsFragment extends Fragment {
                 viewHolder.eventName.setText(model.getEventTitle());
                 viewHolder.eventDescription.setText(model.getEventDesc());
                 viewHolder.tv_time.setText(model.getDate()+" "+model.getTime());
+
+                viewHolder.eventPopupMenu.setOnClickListener(v -> {
+                    PopupMenu popup = new PopupMenu(requireActivity(), viewHolder.eventPopupMenu);
+                    popup.getMenuInflater().inflate(R.menu.event_admin_menu, popup.getMenu());
+                    popup.setOnMenuItemClickListener(item -> {
+                        if(item.getTitle().equals("Delete"))
+                        {
+                            showDialogToDeleteTheEvent(model.getEventId(),model.getIsDeleted());
+                        }
+                        else if(item.getTitle().equals("View details"))
+                        {
+                            showDetails(model.getEventId(),model.getEventLatitude(), model.getEventLongitude());
+                        }
+
+                        return true;
+                    });
+                    popup.show();
+                });
             }
         };
         adapter.startListening();
         recycler_events_admin.setAdapter(adapter);
     }
 
+    private void showDialogToDeleteTheEvent(String eventId, String isDeleted) {
+            //show dialog if isDeleted = 0
+            if(isDeleted.equals("0"))
+            {
+                //Show dialog box with textfield of adding comments and yes and no option.
+                AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+                builder.setTitle("Delete Event");
+                builder.setMessage("Are you sure you want to delete this event?");
+                //Add textfield for why you want to delete the question
+                final EditText input = new EditText(requireActivity());
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                input.setHint("Enter reason");
+                builder.setView(input);
+                builder.setPositiveButton("Yes", (dialog, which) -> {
+                    markEventAsDeleted(eventId,input.getText().toString());
+                });
+                builder.setNegativeButton("No", (dialog, which) -> {
+                    dialog.cancel();
+                });
+                builder.show();
+            }
+            else
+            {
+                Toast.makeText(requireActivity(), "Event already deleted", Toast.LENGTH_SHORT).show();
+            }
+    }
+
+    //Function required for action show details
+    private void showDetails(String eventId, Double latitude,Double longitude)
+    {
+        Intent intent = new Intent(requireActivity(), EventInfo.class);
+        intent.putExtra("eventId", eventId);
+        //Convert double to string
+        intent.putExtra("latitude", String.valueOf(latitude));
+        intent.putExtra("longitude", String.valueOf(longitude));
+        startActivity(intent);
+    }
+
+    //Function which actually mark question as deleted
+    private void markEventAsDeleted(String eventId, String reason)
+    {
+        if (TextUtils.isEmpty(reason)) {
+            Toast.makeText(requireActivity(), "Please enter reason to delete", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        db.collection("Events").document(eventId).update("isDeleted",1);
+        db.collection("Events").document(eventId).update("deletedReason",reason);
+        Toast.makeText(requireActivity(), "Event deleted", Toast.LENGTH_SHORT).show();
+    }
+
     public static class FiltersViewHolder extends RecyclerView.ViewHolder {
         View mView;
         TextView eventName,tv_time,eventDescription;
         RelativeLayout cardLayout;
+        ImageButton eventPopupMenu;
 
         FiltersViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -114,6 +191,7 @@ public class EventsFragment extends Fragment {
             eventDescription = mView.findViewById(R.id.eventDescription);
             tv_time = mView.findViewById(R.id.tv_time);
             cardLayout = mView.findViewById(R.id.cardLayout);
+            eventPopupMenu = mView.findViewById(R.id.eventPopupMenu);
         }
     }
 
--- app/src/main/java/com/example/geopedia/adminmenu/Information/locations/LocationsFragment.java
+++ app/src/main/java/com/example/geopedia/adminmenu/Information/locations/LocationsFragment.java
@@ -1,21 +1,35 @@
 package com.example.geopedia.adminmenu.Information.locations;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.example.geopedia.Info.EventInfo;
 import com.example.geopedia.R;
 import com.example.geopedia.databinding.FragmentLocationsBinding;
 import com.example.geopedia.extras.CustomLocation;
+import com.example.geopedia.showLocations;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
@@ -117,6 +131,25 @@ public class LocationsFragment extends Fragment {
                     viewHolder.lottieAnimationLocation.playAnimation();
                     viewHolder.locationStatus.setText("Deleted");
                 }
+
+                //get the popup menu
+                viewHolder.locationPopupMenu.setOnClickListener(v -> {
+                    PopupMenu popup = new PopupMenu(requireActivity(), viewHolder.locationPopupMenu);
+                    popup.getMenuInflater().inflate(R.menu.event_admin_menu, popup.getMenu());
+                    popup.setOnMenuItemClickListener(item -> {
+                        if(item.getTitle().equals("Delete"))
+                        {
+                            showDialogToDelete(model.getLocationId(),model.getIsDeleted());
+                        }
+                        else if(item.getTitle().equals("View details"))
+                        {
+                            showDetails(model.getLocationId(),model.getLocationLatitude(), model.getLocationLongitude());
+                        }
+
+                        return true;
+                    });
+                    popup.show();
+                });
             }
         };
         adapter.startListening();
@@ -127,6 +160,7 @@ public class LocationsFragment extends Fragment {
         View mView;
         LottieAnimationView lottieAnimationLocation;
         TextView locationName,locationCategory,locationStatus,submittedBy;
+        ImageButton locationPopupMenu;
 
         FiltersViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -136,7 +170,7 @@ public class LocationsFragment extends Fragment {
             locationStatus = mView.findViewById(R.id.locationStatus);
             submittedBy = mView.findViewById(R.id.submittedBy);
             lottieAnimationLocation = mView.findViewById(R.id.lottieAnimationLocation);
-
+            locationPopupMenu = mView.findViewById(R.id.locationPopupMenu);
         }
     }
 
@@ -144,5 +178,82 @@ public class LocationsFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void showDialogToDelete(String locationId, String isDeleted) {
+        //show dialog if isDeleted = 0
+        if(isDeleted.equals("0"))
+        {
+            //Show dialog box with textfield of adding comments and yes and no option.
+            AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+            builder.setTitle("Delete Event");
+            builder.setMessage("Are you sure you want to delete this event?");
+            //Add textfield for why you want to delete the question
+            final EditText input = new EditText(requireActivity());
+            input.setInputType(InputType.TYPE_CLASS_TEXT);
+            input.setHint("Enter reason");
+            builder.setView(input);
+            builder.setPositiveButton("Yes", (dialog, which) -> {
+                markEventAsDeleted(locationId,input.getText().toString());
+            });
+            builder.setNegativeButton("No", (dialog, which) -> {
+                dialog.cancel();
+            });
+            builder.show();
+        }
+        else
+        {
+            Toast.makeText(requireActivity(), "Event already deleted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Function required for action show details
+    private void showDetails(String locationId, Double latitude,Double longitude)
+    {
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        @SuppressLint("MissingPermission")
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        Intent intent = new Intent(requireActivity(), EventInfo.class);
+        intent.putExtra("location_id", locationId);
+        double dis = calculateDistance(currentLatitude,currentLongitude,latitude,longitude);
+        //convert double value into 0.00 format
+        String distance = String.format("%.2f", dis);
+        intent.putExtra("distance", distance);
+        //Convert double to string
+        intent.putExtra("latitude", String.valueOf(latitude));
+        intent.putExtra("longitude", String.valueOf(longitude));
+        startActivity(intent);
+    }
+
+    //Function which actually mark question as deleted
+    private void markEventAsDeleted(String locationId, String reason)
+    {
+        if (TextUtils.isEmpty(reason)) {
+            Toast.makeText(requireActivity(), "Please enter reason to delete", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        db.collection("Locations").document(locationId).update("isDeleted",1);
+        db.collection("Locations").document(locationId).update("deletedReason",reason);
+        Toast.makeText(requireActivity(), "Location deleted", Toast.LENGTH_SHORT).show();
+    }
+
+    private double calculateDistance(double currentLatitude, double currentLongitude, double latitude, double longitude) {
+        double theta = currentLongitude - longitude;
+        double dist = Math.sin(deg2rad(currentLatitude)) * Math.sin(deg2rad(latitude)) + Math.cos(deg2rad(currentLatitude)) * Math.cos(deg2rad(latitude)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        dist = dist * 1.609344;
+        return (dist);
+    }
+
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
     }
 }
